b8b370f0e13d96ea9aeb6d609bad09b02d562ea3
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-02 15:24:32 -0500

    create db if not there

--- twoface/db/core.py
+++ twoface/db/core.py
@@ -1,14 +1,22 @@
 from __future__ import division, print_function
 
+# Standard library
+import os
+
 # Third-party
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import OperationalError
+
+# Project
+from ..log import log
 
 Session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
 Base = declarative_base()
 
-def db_connect(host="localhost", port=5432, user=None, dbname=None, password=None):
+def db_connect(host="localhost", port=5432, user=None, dbname=None, password=None,
+               ensure_db_exists=True):
     """
     Establish a connection to the Postgres database.
     After running :func:`connect`, sessions can be established.
@@ -38,6 +46,26 @@ def db_connect(host="localhost", port=5432, user=None, dbname=None, password=Non
                                                                            password=password)
 
     engine = create_engine(url)
+    try:
+        conn = engine.connect()
+        conn.execute("select * from information_schema.tables")
+        log.debug("Database '{}' exists".format(dbname))
+
+    except OperationalError:
+        if ensure_db_exists:
+            log.info("Database '{}' does not exist -- creating for you...".format(dbname))
+
+            # make sure the database exists
+            _url = os.path.join(os.path.dirname(str(url)), 'postgres')
+            _engine = create_engine(_url)
+            _conn = _engine.connect()
+            _conn.execute("commit")
+            _conn.execute("CREATE DATABASE {0}".format(dbname))
+            _conn.close()
+            del _engine
+        else:
+            raise
+
     Session.configure(bind=engine)
     Base.metadata.bind = engine
 
