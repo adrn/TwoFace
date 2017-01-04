38d91a2790a88ca85870213d257645664307bc67
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-03 20:48:25 -0500

    add trivial test

--- twoface/db/tests/test_database.py
+++ twoface/db/tests/test_database.py
@@ -9,7 +9,8 @@ import pytest
 
 # Project
 from ...config import conf
-from ..core import db_connect
+from ..core import db_connect, Session
+from ..model import AllStar, AllVisit, StarResult, Status, JokerRun
 
 if 'CI' in os.environ and os.environ['CI']:
     # when in continuous integration, only run database tests on Travis
@@ -38,7 +39,20 @@ class TestDB(object):
 
     @pytest.mark.skipif(skip_db_tests, reason="skipping database tests")
     def test_one(self):
-        pass
+        # a target in both test FITS files included in the repo
+        test_target_ID = "4264.2M00000032+5737103"
+
+        session = Session()
+
+        # get star entry and check total num of visits
+        star = session.query(AllStar).filter(AllStar.target_id == test_target_ID).one()
+        assert len(star.visits) == 3
+
+        # get a visit and check that it has one star
+        visit = session.query(AllVisit).filter(AllVisit.target_id == test_target_ID).limit(1).one()
+        assert len(visit.stars) == 1
+
+        session.close()
 
     def teardown(self):
         # delete_db = conf['testing']['delete_test_db']
