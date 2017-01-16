394ff25e3304dc03d3f282b3f3299b69622b6c50
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-16 11:51:42 -0500

    adapt for sqlite instead

--- twoface/db/core.py
+++ twoface/db/core.py
@@ -7,29 +7,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import OperationalError
 
 # Project
-from ..log import log
+from ..log import log as logger
 
 Session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
 Base = declarative_base()
 
-def db_connect(host="localhost", port=5432, user=None, database=None, password=None,
-               ensure_db_exists=True):
+def db_connect(database_path, ensure_db_exists=True):
     """
-    Connect to the specified postgres database.
+    Connect to the specified database.
 
     Parameters
     ----------
-    host : str (optional)
-        Hostname.
-    port : int (optional)
-        Server port.
-    user : str (optional)
-        Username.
-    password : str (optional)
-        Password.
     database : str (optional)
         Name of database
     ensure_db_exists : bool (optional)
@@ -41,33 +31,7 @@ def db_connect(host="localhost", port=5432, user=None, database=None, password=N
         The sqlalchemy database engine.
     """
 
-    url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(host=host,
-                                                                           port=port,
-                                                                           user=user,
-                                                                           database=database,
-                                                                           password=password)
-
-    engine = create_engine(url)
-    try:
-        conn = engine.connect()
-        conn.execute("select * from information_schema.tables")
-        log.debug("Database '{}' exists".format(database))
-
-    except OperationalError:
-        if ensure_db_exists:
-            log.info("Database '{}' does not exist -- creating for you...".format(database))
-
-            # make sure the database exists
-            _url = os.path.join(os.path.dirname(str(url)), 'postgres')
-            _engine = create_engine(_url)
-            _conn = _engine.connect()
-            _conn.execute("commit")
-            _conn.execute("CREATE DATABASE {0}".format(database))
-            _conn.close()
-            del _engine
-        else:
-            raise
-
+    engine = create_engine("sqlite:///{}".format(os.path.abspath(database_path)))
     Session.configure(bind=engine)
     Base.metadata.bind = engine
 
