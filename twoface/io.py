8c5d29d40e9a357eb26668b90b5ab332255c428e
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-08-07 15:23:16 +0200

    code to load samples from an hdf5 file and make diagnostic plots

---
+++ twoface/io.py
@@ -0,0 +1,40 @@
+# Third-party
+import h5py
+from thejoker.utils import quantity_from_hdf5
+
+__all__ = ['load_samples']
+
+def load_samples(group_or_filename, apogee_id=None):
+    """Load posterior samples from The Joker into a dictionary.
+
+    Parameters
+    ----------
+    group_or_filename : :class:`h5py.Group` or str
+    apogee_id : str, optional
+        If a filename is passed to ``group_or_filename``, you must also specify
+        the APOGEE ID of the source to load.
+    """
+
+    if isinstance(group_or_filename, str):
+        if apogee_id is None:
+            raise ValueError("If a filename is passed, you must also specify "
+                             "the APOGEE ID of the source to load.")
+
+        f = h5py.File(group_or_filename)
+        group = f[apogee_id]
+
+    else:
+        f = None
+        group = group_or_filename
+
+    samples_dict = dict()
+    for k in group.keys():
+        if k == 'ln_prior_probs': # skip
+            continue
+
+        samples_dict[k] = quantity_from_hdf5(group, k)
+
+    if f is not None:
+        f.close()
+
+    return samples_dict
---
+++ twoface/plot.py
@@ -0,0 +1,99 @@
+# Third-party
+from astropy.time import Time
+import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+
+from thejoker.plot import plot_rv_curves
+from thejoker.sampler.utils import get_ivar
+
+__all__ = []
+
+def plot_data_orbits(data, samples_dict, n_orbits=128, jitter=None,
+                     xlim_choice='wide', n_times=4096, title=None):
+    """
+    Plot the APOGEE RV data vs. time and orbits computed from The Joker samples.
+
+    Parameters
+    ----------
+    data : :class:`~twoface.data.APOGEERVData`
+        The radial velocity data.
+    samples_dict : dict
+        Dictionary of posterior samples from The Joker.
+    n_orbits : int, optional
+        Number of orbits to plot over the data.
+    jitter : :class:`~astropy.units.Quantity`, optional
+        The jitter used to do the sampling. Only relevant if the jitter was
+        fixed. Used to inflate the error bars for the data.
+    xlim_choice : str, optional
+        Multiple options for how to set the x-axis limits for the plot.
+        ``xlim_choice = 'wide'`` sets the xlim to be twice the longest period
+        sample.
+        ``xlim_choice = 'tight'`` sets the xlim to be twice the time span of the
+        data.
+
+    """
+
+    if jitter is not None:
+        data = data.copy()
+
+        ivar = get_ivar(data, jitter.to(data.rv.unit).value)
+        data.ivar = ivar / data.rv.unit**2
+
+    fig, ax = plt.subplots(1, 1, figsize=(12,6))
+
+    now = Time.now()
+
+    if xlim_choice == 'tight': # twice the span of the data
+        w = np.ptp(data.t.mjd)
+        t_grid = np.linspace(data.t.mjd.min() - w*0.05,
+                             data.t.mjd.max() + w*1.05,
+                             n_times)
+
+    elif xlim_choice == 'wide': # twice the longest period sample
+        t_min = data.t.mjd.min()
+        t_max = max(data.t.mjd.min() + 2*samples_dict['P'].max().value,
+                    data.t.mjd.max())
+        span = t_max - t_min
+        t_grid = np.linspace(t_min - 0.05*span, t_max + 0.05*span, 2048)
+
+    else:
+        raise ValueError('Invalid xlim_choice {0}. Can be "wide" or "tight".'
+                         .format(xlim_choice))
+
+    plot_rv_curves(samples_dict, t_grid, rv_unit=u.km/u.s, data=data, ax=ax,
+                   n_plot=min(len(samples_dict['P']), n_orbits),
+                   plot_kwargs=dict(color='#aaaaaa', alpha=0.25, linewidth=0.5),
+                   data_plot_kwargs=dict(zorder=5, elinewidth=1,))
+
+    # Darken the shortest period sample
+    dark_style = dict(color='#333333', alpha=0.5, linewidth=1, zorder=10)
+
+    P_min_samples = dict()
+    _ix = samples_dict['P'].argmin()
+    for k,v in samples_dict.items():
+        P_min_samples[k] = v[_ix:_ix+1]
+    plot_rv_curves(P_min_samples, t_grid, rv_unit=u.km/u.s, ax=ax,
+                   n_plot=1, plot_kwargs=dark_style)
+
+    # Darken the longest period sample
+    P_max_samples = dict()
+    _ix = samples_dict['P'].argmax()
+    for k,v in samples_dict.items():
+        P_max_samples[k] = v[_ix:_ix+1]
+    plot_rv_curves(P_max_samples, t_grid, rv_unit=u.km/u.s, ax=ax,
+                   n_plot=1, plot_kwargs=dark_style)
+
+    if now.mjd < t_grid.max():
+        ax.axvline(now.mjd, zorder=-10, color='#31a354', alpha=0.4)
+
+    ax.set_xlim(t_grid.min(), t_grid.max())
+
+    _rv =data.rv.to(u.km/u.s).value
+    h = np.ptp(_rv)
+    ax.set_ylim(_rv.min()-2*h, _rv.max()+2*h)
+
+    if title is not None:
+        ax.set_title(title)
+
+    return fig
