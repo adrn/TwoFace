6c7e9ab32d83bb15feb36201c21df1eb7e4c8c3c
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-11-10 00:26:10 -0500

    more time

--- scripts/exp1/run.sh
+++ scripts/exp1/run.sh
@@ -3,7 +3,7 @@
 #SBATCH -o apogee.o%j             # output file name (%j expands to jobID)
 #SBATCH -e apogee.e%j             # error file name (%j expands to jobID)
 #SBATCH -n 224
-#SBATCH -t 2:00:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH -t 8:00:00             # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
