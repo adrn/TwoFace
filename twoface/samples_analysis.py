eb4e525b0981df07556cd08f8f1c7d514c776f96
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-03-02 09:40:50 -0500

    Utility to get the max likelihood sample

--- twoface/samples_analysis.py
+++ twoface/samples_analysis.py
@@ -2,7 +2,7 @@
 import astropy.units as u
 import numpy as np
 
-__all__ = ['unimodal_P']
+__all__ = ['unimodal_P', 'max_likelihood_sample']
 
 
 def unimodal_P(samples, data):
@@ -23,3 +23,23 @@ def unimodal_P(samples, data):
     delta = 4*P_min**2 / (2*np.pi*T)
 
     return np.std(P_samples) < delta
+
+
+def max_likelihood_sample(data, samples):
+    """Return the maximum-likelihood sample.
+
+    Parameters
+    ----------
+    data : `~thejoker.RVData`
+    samples : `~thejoker.JokerSamples`
+
+    """
+    chisqs = np.zeros(len(samples))
+
+    for i in range(len(samples)):
+        orbit = samples.get_orbit(i)
+        residual = data.rv - orbit.radial_velocity(data.t)
+        err = np.sqrt(data.stddev**2 + samples['jitter'][i]**2)
+        chisqs[i] = np.sum((residual**2 / err**2).decompose())
+
+    return samples[np.argmin(chisqs)]
